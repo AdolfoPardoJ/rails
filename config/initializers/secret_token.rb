5fa79bebb3450330f0b4a31e0815ec87ffc41808
Adolfo Pardo <adolfopardo.j31@gmail.com>
2013-10-22 20:41:36 -0200

    Primer comentario

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adolforails::Application.config.secret_token = 'f7e30671b7dff0e97b20ec26d9eecf0233cdbd236645266d1b14fce3275d76659658785948725f33e12379e59a1b56ff3473045bc92dce7c31ec384eb66437ad'
